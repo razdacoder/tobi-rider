12065daba1d2540288d73e54d6650a086ddd3688
Rasheed Ramon <ramonrash2@gmail.com>
2023-11-15 12:28:20 +0100

    Changed Icon

--- src/screens/Splash.jsx
+++ src/screens/Splash.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { MdLocalTaxi } from "react-icons/md";
+import { MdElectricBike, MdLocalTaxi } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 
 const Splash = () => {
@@ -15,7 +15,8 @@ const Splash = () => {
   }, []);
   return (
     <div className="bg-hero flex justify-center flex-col items-center h-full">
-      <h3 className="text-3xl font-bold text-blue-500">Polyride</h3>
+      <MdElectricBike size={80} className="text-green-500" />
+      <h3 className="text-3xl font-bold text-blue-500">Tobi Driver</h3>
     </div>
   );
 };
